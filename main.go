772ebb43abfd56e18cdd178b70c2a729489d915f
Distortions81 <carlotto81@gmail.com>
2023-10-16 12:14:40 -0600

    init ebiten

--- main.go
+++ main.go
@@ -1,6 +1,39 @@
 package main
 
-func main() {
+import "github.com/hajimehoshi/ebiten/v2"
 
-	//
+type Game struct {
+}
+
+func main() {
+	/* Set up ebiten and window */
+	ebiten.SetVsyncEnabled(true)
+	ebiten.SetTPS(ebiten.SyncWithFPS)
+	ebiten.SetScreenClearedEveryFrame(true)
+	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeDisabled)
+	ebiten.SetWindowSize(1024, 1024)
+	if err := ebiten.RunGameWithOptions(newGame(), &ebiten.RunGameOptions{GraphicsLibrary: ebiten.GraphicsLibraryOpenGL}); err != nil {
+		return
+	}
+}
+
+func newGame() *Game {
+
+	/* Initialize the game */
+	return &Game{}
+}
+
+/* Ebiten: Draw everything */
+func (g *Game) Draw(screen *ebiten.Image) {
+}
+
+/* Ebiten resize handling */
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return int(outsideWidth), int(outsideHeight)
+}
+
+/* Input interface handler */
+func (g *Game) Update() error {
+
+	return nil
 }
