198d7dd001240698900a088a05f23325742fe6dd
Distortions81 <carlotto81@gmail.com>
2023-10-19 19:49:58 -0600

    build script

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "compiling..."
+
+GOOS=js GOMAXPROCS=1 GOARCH=wasm go build -tags=ebitensinglethread -pgo=auto -trimpath -gcflags=all="-B" -ldflags="-s -w" -o main.wasm
+
+echo "compressing..."
+rm main.wasm.gz
+gzip -9 main.wasm
+
+cp main.wasm.gz ../goMMOServ/www/
+echo "copied..."
