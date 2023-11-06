b5c2a92fd858513fa287848a6daf7f2b2f25e412
Distortions81 <carlotto81@gmail.com>
2023-11-05 17:14:46 -0700

    .

--- main.go
+++ main.go
@@ -135,11 +135,15 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 		halfScreenY = outsideHeight / 2
 
 		//Keep UI windows from going outside the window
-		if screenX < 720 || screenY < 720 {
+		if !smallMode && (screenX < 720 || screenY < 720) {
 			smallMode = true
 			//uiScale = 0.5
 			maxScreenSize = 1080 / 2
 			ebiten.SetWindowSize(screenX/2, screenY/2)
+		} else if smallMode && (screenX >= 720 || screenY >= 720) {
+			smallMode = false
+			//uiScale = 0.5
+			maxScreenSize = 1080
 		}
 
 		clampUIWindows()
