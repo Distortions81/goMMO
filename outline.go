29308a9890c1f7cd9c651e71dd1803c42078203a
Distortions81 <carlotto81@gmail.com>
2023-11-28 20:13:43 -0700

    outline start

---
+++ outline.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/draw"
+
+	"github.com/fogleman/gg"
+)
+
+func makeOutlines(inputImg image.Image) image.Image {
+
+	// Create a new image context for the original image
+	originalContext := gg.NewContextForImage(inputImg)
+
+	// Create a new RGBA image for the outlined image
+	outlinedRect := originalContext.Image().Bounds()
+	outlinedRGBA := image.NewRGBA(outlinedRect)
+
+	// Define multiple outline colors
+	outlineColors := []color.RGBA{
+		{0, 255, 255, 255},
+		{0, 255, 255, 255},
+		{0, 192, 255, 255},
+		{0, 128, 255, 255},
+		{0, 0, 255, 255},
+	}
+
+	// Set the outline width
+	oWidth := 1.25
+	outlineWidth := float64(len(outlineColors)+1) * oWidth
+
+	// Draw multiple outlines based on alpha channel
+	for _, outlineColor := range outlineColors {
+		outlinedContext := gg.NewContextForRGBA(outlinedRGBA)
+		outlineWidth -= oWidth
+		outlinedContext.SetLineWidth(float64(outlineWidth))
+		outlinedContext.SetColor(outlineColor)
+
+		for y := outlinedRect.Min.Y; y < outlinedRect.Max.Y; y++ {
+			for x := outlinedRect.Min.X; x < outlinedRect.Max.X; x++ {
+				_, _, _, a := inputImg.At(x, y).RGBA()
+				if a > 0 {
+					outlinedContext.DrawRectangle(float64(x), float64(y), 1, 1)
+					outlinedContext.Stroke()
+				}
+			}
+		}
+
+	}
+
+	// Composite the original image on top of the outlined image
+	draw.Draw(outlinedRGBA, outlinedRect, originalContext.Image(), image.Point{}, draw.Over)
+
+	return outlinedRGBA
+}
