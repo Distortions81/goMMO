37eb59e0b0d9b8fba76f6b198653d7c56625357e
Distortions81 <carlotto81@gmail.com>
2023-11-30 12:32:50 -0700

    start of new effects

--- sprite-pack.go
+++ sprite-pack.go
@@ -1,10 +1,34 @@
 package main
 
 import (
+	"image/color"
+
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
 var spritePacks map[string]*spritePack
+
+var healAnimation = colorPack{
+	frames: []outlineColors{
+		{
+			colors: []color.RGBA{
+				{R: 1, G: 0, B: 0, A: 1},
+				{R: 0, G: 1, B: 0, A: 1},
+				{R: 0, G: 0, B: 1, A: 1},
+			},
+			outlineWidth: []int{2},
+		},
+		{
+			colors: []color.RGBA{
+				{R: 0, G: 0, B: 1, A: 1},
+				{R: 0, G: 1, B: 0, A: 1},
+				{R: 1, G: 0, B: 0, A: 1},
+			},
+			outlineWidth: []int{2},
+		},
+	},
+	numFrames: 2,
+}
 
 func initSpritePacks() {
 	spritePacks = make(map[string]*spritePack)
@@ -18,12 +42,21 @@ func initSpritePacks() {
 				walkSprite := findItemImage(itemType.name, item.name, "walk")
 				deadSprite := findItemImage(itemType.name, item.name, "dead")
 				attackSprite := findItemImage(itemType.name, item.name, "attack")
+
+				healSprite := makeOutlines(walkSprite, healAnimation.frames[0].colors)
+				healDeadSprite := makeOutlines(deadSprite, healAnimation.frames[0].colors)
+				healAttackSprite := makeOutlines(attackSprite, healAnimation.frames[0].colors)
+
 				if walkSprite == nil || deadSprite == nil || attackSprite == nil {
 					doLog(true, "Item not found: %v, %v", itemType.name, item.name)
 					continue
 				}
-				newPack := &spritePack{walking: walkSprite, dead: deadSprite, attack: attackSprite,
-					sizeH: int(item.sizeH), sizeW: int(item.sizeW)}
+				newPack := &spritePack{
+					walking: walkSprite, dead: deadSprite, attack: attackSprite,
+					healing:       []*ebiten.Image{healSprite},
+					healingDead:   []*ebiten.Image{healDeadSprite},
+					healingAttack: []*ebiten.Image{healAttackSprite},
+					sizeH:         int(item.sizeH), sizeW: int(item.sizeW)}
 				spritePacks[item.name] = newPack
 			}
 		}
@@ -37,14 +70,9 @@ type spritePack struct {
 	dead    *ebiten.Image
 	attack  *ebiten.Image
 
-	healing  *ebiten.Image
-	healing2 *ebiten.Image
-
-	healingDead  *ebiten.Image
-	healingDead2 *ebiten.Image
-
-	healingAttack  *ebiten.Image
-	healingAttack2 *ebiten.Image
+	healing       []*ebiten.Image
+	healingDead   []*ebiten.Image
+	healingAttack []*ebiten.Image
 
 	healColors colorPack
 }
